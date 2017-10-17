f6bdaa1bf01b87a2632fadb5c1e02dc83d1dda9f
unknown <nolanholden@users.noreply.github.com>
2017-10-17 01:32:03 -0400

    add a ternary type

---
+++ sstring_ternary.c
@@ -0,0 +1,5 @@
+#include "sstring_ternary.h"
+
+static const sstring_ternary sstring_ternary_down = -1;
+static const sstring_ternary sstring_ternary_middle = 0;
+static const sstring_ternary sstring_ternary_up = 1;
---
+++ sstring_ternary.h
@@ -0,0 +1,10 @@
+#ifndef _SSTRING_TERNARY_H_
+#define _SSTRING_TERNARY_H_
+
+typedef signed char sstring_ternary;
+
+extern const sstring_ternary sstring_ternary_down;
+extern const sstring_ternary sstring_ternary_middle;
+extern const sstring_ternary sstring_ternary_up;
+
+#endif // _SSTRING_TERNARY_H_
