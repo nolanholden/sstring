9d7733d21fe853c539a002ea97c99d9a20edb824
Nolan Holden <nolanholden@users.noreply.github.com>
2017-10-17 01:35:09 -0400

    add bool type for memory save and configurability

---
+++ sstring_bool.c
@@ -0,0 +1,4 @@
+#include "sstring_bool.h"
+
+static const sstring_bool sstring_bool_false = 0;
+static const sstring_bool sstring_bool_true = 1;
---
+++ sstring_bool.h
@@ -0,0 +1,9 @@
+#ifndef _SSTRING_BOOL_H_
+#define _SSTRING_BOOL_H_
+
+typedef unsigned char sstring_bool;
+
+extern const sstring_bool sstring_bool_false;
+extern const sstring_bool sstring_bool_true;
+
+#endif // _SSTRING_BOOL_H_
